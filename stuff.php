6ddbb80b205067eb2c9ae6430865dee5028520c3
Willie Scott <scottwe28@mounties.mansfield.edu>
2017-08-21 15:16:04 -0400

    adding stuff

---
+++ stuff.php
@@ -0,0 +1,15 @@
+   <?php
+  
+
+$attribute = array("Water ", "Dark ","Wind ","Fire ","Earth ","Light ");
+$weapon = array("twin-swords ", "sword and shield ", "sword and element ","Mace-hammer ","double blade ", "katana ");
+$character = array("Samurai ", "Monk ", "Dragoon ", "Ninja ", "Barbarian ", "Knight ", "Necomancer ");
+
+$attribute[array_rand($attribute)];
+ $weapon[array_rand($weapon)];
+ $character[array_rand($character)];
+
+
+echo $story = 'The legendary $character now wields the ancient  $weapon of destruction which channels the power of $attibute so e can try conquer all.';
+
+ ?>
